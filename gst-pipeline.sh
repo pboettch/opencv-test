315e233102f44beeb16526cafb64075931c39d21
Patrick Boettcher <patrick.boettcher@posteo.de>
2016-08-09 08:28:05 +0200

    gst-pipeline with dmabuf

--- gst-pipeline.sh
+++ gst-pipeline.sh
@@ -1,9 +1,10 @@
 export GST_PLUGIN_PATH=.
 
-	gst-launch-1.0 -vvv \
-		v4l2src io-mode=4 ! \
-		'video/x-raw,format=(string)YUY2,width=640,height=480' ! \
-		videoconvert ! \
-		myfilter ! \
-		videoconvert ! \
-		autovideosink
+gst-launch-1.0 -vvv \
+	v4l2src io-mode=4 ! \
+	'video/x-raw, width=640, height=360, format=YUY2' ! \
+	myfilter ! \
+	xvimagesink sync=false -vvv
+
+#		videoconvert ! \
+#		videoconvert ! \
