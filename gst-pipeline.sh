dbc73316bc64ead9e86f37c21131d6dde1a49f7c
Patrick Boettcher <patrick.boettcher@posteo.de>
2016-08-17 18:21:10 +0200

    gst-pipeline insert myfilter

--- gst-pipeline.sh
+++ gst-pipeline.sh
@@ -1,9 +1,10 @@
 export GST_PLUGIN_PATH=.
 
 # io-mode=0 ! \
-gst-launch-1.0 -v --gst-debug=3,myfilter*:6 \
+gst-launch-1.0 -v --gst-debug=3 \
 	v4l2src io-mode=4 ! \
 	'video/x-raw, width=1280, height=720, format=YUY2' ! \
+	myfilter ! \
 	videoconvert ! \
 	autovideosink
 
